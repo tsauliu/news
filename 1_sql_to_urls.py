98324368f8aced594a4865fb511577498467e60f
leo <caoliu1994@gmail.com>
2025-04-17 19:58:03 +0800

    new way to generate wechat urls

--- 1_sql_to_urls.py
+++ 1_sql_to_urls.py
@@ -1,10 +1,32 @@
+'''
+在远程服务器上host一个文件服务器,可以access folder
+TODO: 1. 合并wewe-rss.db 和我新抓的db，生成新db，只看过去7天发布的文章
+'''
 # %%
 # extract news from sqlite
 import sqlite3
 import pandas as pd
 import os
-from parameters import friday_date
+from parameters import friday_date, download_file
 from apikey import news_start
+
+import requests
+import shutil
+
+# URL of the remote database file
+local_db_path = 'data/wechat_articles.db'
+
+remote_db_url = "http://118.193.44.18:8000/data/wechat_articles.db"
+os.makedirs('data', exist_ok=True)
+download_file(remote_db_url, local_db_path)
+
+conn_wechat = sqlite3.connect(local_db_path)
+wechat_articles = pd.read_sql_query("SELECT * FROM articles", conn_wechat)
+conn_wechat.close()
+wechat_articles['pub_time']=pd.to_datetime(wechat_articles['pub_time'],format='%Y年%m月%d日 %H:%M').dt.strftime('%Y-%m-%d %H:%M:%S')
+wechat_articles['source'] = 'wechat'
+wechat_articles.rename(columns={'pub_time':'publish_time','article_title':'title','channel_scraped':'mp_name'}, inplace=True)
+wechat_articles=wechat_articles[['mp_name', 'title', 'url', 'publish_time','source']].sort_values(by='publish_time', ascending=False)
 
 conn = sqlite3.connect('data/wewe-rss.db')
 articles = pd.read_sql_query("SELECT * FROM articles", conn)
@@ -17,10 +39,9 @@ feeds = pd.read_sql_query("SELECT * FROM feeds", conn)
 
 article_clean=articles[['mp_id', 'title','publish_time', 'url']].merge(feeds.rename(columns={'id': 'mp_id'})[['mp_id', 'mp_name']], on='mp_id', how='left').drop(columns=['mp_id'])
 article_clean.sort_values(by='publish_time', ascending=False, inplace=True)
-
-article_clean = article_clean[pd.to_datetime(article_clean['publish_time']) >= (pd.to_datetime(friday_date) - pd.Timedelta(days=news_start))]
-
-
+article_clean['source'] = 'wewerss'
+article_clean = pd.concat([article_clean, wechat_articles]).drop_duplicates(subset=['url'])
+article_clean = article_clean[pd.to_datetime(article_clean['publish_time']) >= (pd.to_datetime(friday_date) - pd.Timedelta(days=news_start))].sort_values(by='publish_time', ascending=False)
 folder_path = f'data/1_urls/'
 os.makedirs(folder_path, exist_ok=True)
 
--- parameters.py
+++ parameters.py
@@ -1,9 +1,22 @@
 # Calculate the date of the Friday of the current week
 #%%
 import datetime
+import requests
+import shutil
 
 current_date = datetime.datetime.now()
 days_until_friday = (6 - (current_date.weekday() + 2) % 7) % 7  # 6 represents Friday in this system
 friday_date = (current_date + datetime.timedelta(days=days_until_friday)).strftime('%Y-%m-%d')
 errorkeywords=["微信，是一个生活方式","参数错误","LinkReader","微信输入法"]
 sector_list=['商业落地','核心技术','政策监管','企业战略','硬件设备','数据与地图','资本动向']#%%
+
+
+def download_file(url, local_path):        # Download the database file
+    print(f"Downloading database from {url}...")
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(local_path, 'wb') as f:
+            shutil.copyfileobj(response.raw, f)
+        print(f"Database successfully downloaded to {local_path}")
+    else:
+        print(f"Failed to download database. Status code: {response.status_code}")
