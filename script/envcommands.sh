9875bab63c9ac08a55945b8de8d3c41eba3989aa
leo <leo@gcp.tw>
2025-04-17 12:35:30 +0000

    more logging

--- script/envcommands.sh
+++ script/envcommands.sh
@@ -1,3 +1,6 @@
 # python3 -m venv pyenv
 source ~/pyenv/bin/activate
 pip install -r ~/Dropbox/tradingcode/tools/envs/requirements.txt
+
+git config --global user.name "leo"
+git config --global user.email "leo@gcp.tw"
