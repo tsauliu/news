11a2af052b0344c582f9690d208bb0b0dee8e416
leo <leo@gcp.tw>
2025-04-17 12:35:21 +0000

    Update project paths in scripts and add 'requests' to requirements

---
+++ script/envcommands.sh
@@ -0,0 +1,3 @@
+# python3 -m venv pyenv
+source ~/pyenv/bin/activate
+pip install -r ~/Dropbox/tradingcode/tools/envs/requirements.txt
--- script/get_urls_to_md.sh
+++ script/get_urls_to_md.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-screen -ls | grep 'sqlurl' | cut -d. -f1 | awk '{print $1}' | xargs kill 2>/dev/null || true; screen -dmS sqlurl bash -c 'cd ~/wewe-rss && source ~/pyenv/bin/activate && python3 1_sql_to_urls.py; exec bash'
+screen -ls | grep 'sqlurl' | cut -d. -f1 | awk '{print $1}' | xargs kill 2>/dev/null || true; screen -dmS sqlurl bash -c 'cd ~/AutoNews && source ~/pyenv/bin/activate && python3 1_sql_to_urls.py; exec bash'
 sleep 15
-screen -ls | grep 'urlmd' | cut -d. -f1 | awk '{print $1}' | xargs kill 2>/dev/null || true; screen -dmS urlmd bash -c 'cd ~/wewe-rss && source ~/pyenv/bin/activate && python3 2_url_to_md.py; exec bash'
+screen -ls | grep 'urlmd' | cut -d. -f1 | awk '{print $1}' | xargs kill 2>/dev/null || true; screen -dmS urlmd bash -c 'cd ~/AutoNews && source ~/pyenv/bin/activate && python3 2_get_mds.py; exec bash'
