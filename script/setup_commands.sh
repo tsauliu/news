84cdd5232fd9969f6e1f39172706a50f75559f42
leo <leo@gcp.tw>
2025-04-18 10:51:20 +0800

    优化prompt

--- script/setup_commands.sh
+++ script/setup_commands.sh
@@ -4,3 +4,5 @@ pip install -r ~/Dropbox/tradingcode/tools/envs/requirements.txt
 
 git config --global user.name "leo"
 git config --global user.email "leo@gcp.tw"
+
+sudo timedatectl set-timezone Asia/Shanghai
