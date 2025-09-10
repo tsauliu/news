206fca879288671cbe2a6bb57775d08368645814
leo <leo@gcp.tw>
2025-09-10 18:21:15 +0800

    update scripts

--- script/crontab.sh
+++ script/crontab.sh
@@ -2,6 +2,6 @@
 # 在crontab中，五个星号分别代表：分钟 小时 日期 月份 星期几
 
 # Run sql_to_urls.py at 15 minutes past every hour from 8 AM to 11 PM
-15 8,11,12,14,16,20,23 * * * ~/AutoNews/script/get_urls_to_md.sh
+15 8,12,16,23 * * * /home/caoliu/Dropbox/BDAcode/AutoNews/script/get_urls_to_md.sh
 # Run mdtosummary every Friday at 13:00
-0 13 * * 5 ~/AutoNews/script/md_to_summary.sh
+0 13 * * 5 /home/caoliu/Dropbox/BDAcode/AutoNews/script/md_to_summary.sh
